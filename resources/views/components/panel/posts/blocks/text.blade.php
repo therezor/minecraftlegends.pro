58dca59fa49b6c1e4f4a56bc8f363cdc46f24870
rezor <zmrezor@gmail.com>
2023-01-21 23:23:43 +0000

    Error fix

--- resources/views/components/panel/posts/blocks/text.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="mb-4">
-    <x-form-input required :name="'post.blocks.' . $key . '.title'" placeholder="{{ trans('attributes.title') }}"/>
-</div>
-
-<div>
-    <x-form-textarea class="text-editor" :name="'post.blocks.' . $key . '.description'" rows="3" placeholder="{{ trans('attributes.description') }}"/>
-</div>
